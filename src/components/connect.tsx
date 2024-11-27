42ebb7cc1e10c119b2028bb1ef9c15412a44dcd4
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2024-11-27 07:37:11 +0530

    feat : added socials with links

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -1,7 +1,11 @@
+'use client'
 import React from 'react'
 import MonoFont from '@/components/ui/mono-font'
 
+import { socials } from '@/constants/socials'
+
 const Connect = () => {
+  const handleOnSocialLinkClick = (link: string) => window.open(link, '_')
   return (
     <div className='pt-6 mb-8 '>
       <MonoFont text={`let's get in touch`} />
@@ -9,7 +13,17 @@ const Connect = () => {
         <div className='text-sm '>{`Got a Vision, a Challenge, or Simply Want to Say Hi?`}</div>
         <div className='text-sm'>{`Let's Build Something Incredible Together!`}</div>
       </div>
-      <div className='text-sm opacity-50'>chaudharyanmol321@gmail.com</div>
+      <div className='text-sm opacity-50'>
+        <a href='mailto:chaudharyanmol321@gmail.com'>chaudharyanmol321@gmail.com</a>
+      </div>
+      <div className='mt-4 mb-3 flex items-center gap-4'>
+        {socials.map(({ Icon, title, link }) => (
+          <Icon onClick={() => handleOnSocialLinkClick(link)} key={title} size={20} className='opacity-50 cursor-pointer hover:opacity-75' />
+        ))}
+      </div>
+      <div onClick={() => handleOnSocialLinkClick('https://cal.com/anmol-chaudhary/30min')} className='flex text-sm opacity-50 underline cursor-pointer hover:opacity-75'>
+        <span>{`Let's talk`}</span>
+      </div>
     </div>
   )
 }
---
+++ src/constants/socials.ts
@@ -0,0 +1,9 @@
+import { Twitter, Dribbble, Instagram, Linkedin, Github } from 'lucide-react'
+
+export const socials = [
+  { title: 'github', link: 'https://github.com/4nmolChaudhary', Icon: Github },
+  { title: 'x', link: 'https://x.com/4nmol_Chaudhary', Icon: Twitter },
+  { title: 'dribbble', link: 'https://dribbble.com/AnmolChaudhary', Icon: Dribbble },
+  { title: 'instagram', link: 'https://www.instagram.com/4nmolchaudhary/', Icon: Instagram },
+  { title: 'linkedin', link: 'https://www.linkedin.com/in/anmol-chaudhary-068082183/', Icon: Linkedin },
+]
