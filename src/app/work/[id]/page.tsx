34c40d7cc92915c3967f06604f2940e55d639c9e
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2024-12-02 11:09:27 +0530

    feat : added work desc and other useful sites

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -18,9 +18,19 @@ export default function Page({ params }: { params: { id: string } }) {
           <div className='mr-3'>Year</div>
           <div className='opacity-50'>{workInfo?.year}</div>
         </div>
+        <div className='flex items-center'>
+          <div className='mr-3'>Website</div>
+          <a className='opacity-50 underline' target='_blank' href={workInfo?.website}>
+            {workInfo?.website}
+          </a>
+        </div>
       </div>
       <div className='w-full aspect-video'>
         <img src={`/${workInfo?.image}`} className='w-full h-full rounded-md border-2 border-slate-400/10 bg-neutral-100 dark:bg-neutral-900' />
+      </div>
+      <div className='mt-2'>
+        <div className='mr-3'>Role and Responsibilities</div>
+        <div className='opacity-50'>{workInfo?.work}</div>
       </div>
     </div>
   )
--- src/constants/work.ts
+++ src/constants/work.ts
@@ -7,17 +7,23 @@ export const work = [
     desc: 'NetAI distinguishes itself in the field of network management by leveraging cutting-edge proprietary AI technology that is meticulously tailored to address the complexities of modern network environments.',
     year: '2024',
     website: 'https://netanalytics.xyz',
+    work: 'As the lead for the complete redesign and development of the web application, I spearheaded the project from end to end, encompassing UI/UX design, architectural planning, and full development. I collaborated closely with team to ensure a scalable, user-centric, and high-performance solution, delivering a seamless and modernized user experience',
+    techstack: [],
   },
   {
     title: 'Real Time compliane scanning for SOC2 for AWS cloud',
     at: 'Compliance Foundry',
     image: 'mockup-shot-2.png',
     id: 'compliance-foundry',
+    year: 'JAN 2024 - APR 2024',
+    website: 'https://cloudscanner.rapidsoc2.com/',
   },
   {
     title: 'Asset management and maintenance for electrical components',
     at: 'eGalvanic',
     image: 'mockup-shot-3.png',
     id: 'e-galvanic',
+    year: '2020 - 2023',
+    website: 'https://democompany.egalvanic.com/',
   },
 ]
