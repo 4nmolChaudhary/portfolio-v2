9f074c0a97e7abcb4a55d4286033844fecc9f712
Anmol Chaudhary <chaudharyanmol321@gmail.com>
2024-12-10 12:46:42 +0530

    feat : added resume for download

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -3,9 +3,18 @@ import React from 'react'
 import MonoFont from '@/components/ui/mono-font'
 
 import { socials } from '@/constants/socials'
+import { ArrowDownToLine } from 'lucide-react'
+import { CV_DOWNLOAD_LINK, CAL_COM_CALL_LINK } from '@/constants/others'
 
 const Connect = () => {
   const handleOnSocialLinkClick = (link: string) => window.open(link, '_')
+  const handleDownloadCV = async () => {
+    try {
+      const link = document.createElement('a')
+      link.href = CV_DOWNLOAD_LINK
+      link.click()
+    } catch (error) {}
+  }
   return (
     <div className='pt-6 mb-8 '>
       <MonoFont text={`let's get in touch`} />
@@ -21,8 +30,14 @@ const Connect = () => {
           <Icon onClick={() => handleOnSocialLinkClick(link)} key={title} size={20} className='opacity-50 cursor-pointer hover:opacity-75' />
         ))}
       </div>
-      <div onClick={() => handleOnSocialLinkClick('https://cal.com/anmol-chaudhary/30min')} className='flex text-sm opacity-50 underline cursor-pointer hover:opacity-75'>
-        <span>{`Let's talk`}</span>
+      <div className='flex items-center gap-3'>
+        <div onClick={() => handleOnSocialLinkClick(CAL_COM_CALL_LINK)} className='flex text-sm opacity-50 underline cursor-pointer hover:opacity-75'>
+          <span>{`Let's talk`}</span>
+        </div>
+        <div onClick={handleDownloadCV} className='flex items-center text-sm opacity-50 underline cursor-pointer hover:opacity-75'>
+          <span>{`CV`}</span>
+          <ArrowDownToLine size={16} className='opacity-50' />
+        </div>
       </div>
     </div>
   )
---
+++ src/constants/others.ts
@@ -0,0 +1,2 @@
+export const CV_DOWNLOAD_LINK = 'https://raw.githubusercontent.com/4nmolChaudhary/Resume/main/CV_ANMOL_CHAUDHARY.pdf'
+export const CAL_COM_CALL_LINK = 'https://cal.com/anmol-chaudhary/30min'
